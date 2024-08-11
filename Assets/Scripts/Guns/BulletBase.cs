a35c8d571373421d81910afe16e4c6374ec5bf08
Frogdig <Jonte_carlberg@hotmail.com>
2024-08-11 15:47:37 +0200

    Smol fix + V1 på impactgrej
    Woaw

--- Assets/Scripts/Guns/BulletBase.cs
+++ Assets/Scripts/Guns/BulletBase.cs
@@ -9,12 +9,27 @@ public abstract class BulletBase : MonoBehaviour
     [SerializeField] protected int damage = 10;
     [SerializeField] protected float lifeTime = 5f;
     [Header("Bullet Specifics")]
-    [SerializeField] protected GameObject hitEffectPrefab;//Gör en lista och bytt till typ partikeleffekter
+    //[SerializeField] protected GameObject hitEffectPrefab;//Gör en lista och bytt till typ partikeleffekter
+    [SerializeField] protected GameObject enemyHitEffectPrefab;
+    [SerializeField] protected GameObject wallHitEffectPrefab;
+    [SerializeField] protected GameObject groundHitEffectPrefab;
+
+    [SerializeField] protected Dictionary<string, GameObject> hitEffectsByTag;
 
     protected Rigidbody rb;
 
-    protected virtual void Start()
+    private void Awake()
     {
+        hitEffectsByTag = new Dictionary<string, GameObject>
+        {
+            { "Enemy", enemyHitEffectPrefab },
+            { "Wall", wallHitEffectPrefab },
+            { "Ground", groundHitEffectPrefab }
+            // Add more tag-prefab pairs as needed
+        };
+    }
+    protected virtual void Start()
+    {        
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
        
--- Assets/Scripts/Guns/BulletTypes/RevolverBullet.cs
+++ Assets/Scripts/Guns/BulletTypes/RevolverBullet.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,25 @@ using UnityEngine;
 public class RevolverBullet : BulletBase
 {
     [SerializeField] private float impactEffectDuration = 2f;
+
     protected override void OnHit(Collider other)
-    {       
+    {
         //Kolla beroende på vad man träffar så byts effekten
-        if (hitEffectPrefab != null)
+        //if (hitEffectPrefab != null)
+        //{
+        //    GameObject impactEffect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+        //    Destroy(impactEffect, impactEffectDuration);
+        //}
+
+        string tag = other.tag;
+        if (hitEffectsByTag.TryGetValue(tag, out GameObject hitEffect))
         {
-            GameObject impactEffect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
-            Destroy(impactEffect, impactEffectDuration);
+            Instantiate(hitEffect, transform.position, Quaternion.identity);
+            //Destroy(hitEffect.gameObject, impactEffectDuration);
+        }
+        else
+        {
+            Debug.LogWarning("No hit effect found for tag: " + tag);
         }
     }
    
